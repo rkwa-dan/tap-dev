98c20a59387e4042d34b57be9ecc4a9e2fc372e8
psg18dhc <116092796+psg18dhc@users.noreply.github.com>
2023-04-17 12:59:49 +0100

    Corrected code for ebs

--- main.tf
+++ main.tf
@@ -7,36 +7,45 @@ resource "tls_private_key" "pk" {
 resource "aws_key_pair" "kp" {
   key_name   = "CMC_Linux_key"     
   public_key = tls_private_key.pk.public_key_openssh
+
+  # # Create "sshKey.pem" to your computer!!
+  # provisioner "local-exec" { 
+  #   command = << EOF
+  #     echo '${tls_private_key.pk.private_key_pem}' > CMC-linux-Key.pem
+  #     chmod 400 CMC_Linux_key.pem
+  #     EOF
+  # }
 }
 
-/*# Create "sshKey.pem" to your computer!!
-provisioner "local-exec" { 
-    command = "echo '${tls_private_key.pk.private_key_pem}' > ./CMC-linux-Key.pem"
+resource "local_file" "CMC_local_key" {
+  content = tls_private_key.pk.private_key_pem
+  filename = "CMC_Linux_key.pem"
+  file_permission = "0600"
 }
- */
 
 data "template_file" "user_data" {
   template = file("./bootstrap.yaml")
 }
 // building dev  vm
 
-module "ec2-instance" {
+module "ec2_instance" {
   # lines below used for the 'ec2_instance' module which is a couple of years old. 
   source  = "terraform-aws-modules/ec2-instance/aws"
-  # version = "~> 3.0"
+  version = "~> 3.0"
 
   #module "ec2" # uses newer 4x xprovider code from hashicorp and allows for the ebs_volume_attachment & creation resources. 
   # source = ""
 // build the requisite number of servers for the partner instances required.
 // instance size can be modified per spec.
 
-  for_each = toset(["one"])
+  # for_each = toset(["one"])
   # for more instances you can create additional instances separated by commast
 #  , "two", "three"])
-  name = "dev-cmc-instance-${each.key}"
+  # name = "dev-cmc-instance-${each.key}"
 
-ami                    = "ami-08970fb2e5767e3b8"  // Rhel 8 ami
- // ami = data.aws_ami.rhel_8_5.id
+  name = "linux-cmc-instance"
+  availability_zone = local.availability_zone
+  ami = data.aws_ami.rhel_8_5.id
   instance_type          = "t3.large"
   key_name               = aws_key_pair.kp.key_name
   monitoring             = true
@@ -51,7 +60,8 @@ ami                    = "ami-08970fb2e5767e3b8"  // Rhel 8 ami
   tags = {
     Terraform   = "true"
     Environment = "dev"
+    application = "cmc-linux"
   }
-
-
+  
 }
+ 
